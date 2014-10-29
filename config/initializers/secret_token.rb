f0a246f24a5f7bade3055263d8d4b4a9ccf7e299
Nan Davenport <nannigan@nannigans.com>
2014-10-29 18:30:11 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catsit::Application.config.secret_key_base = '7508658a380b3b9986f99aa8309fb13b00a8b0d9ebb67fb5c19cad689df700a93bbbd9124e931c55fab72769610eca933c1e7128d3d298fb331267caccb83810'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Catsit::Application.config.session_store :cookie_store, key: '_catsit_session'
